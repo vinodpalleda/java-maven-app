8e658276483fcdc8e6b38081624bd2a75a4795db
vinodpalleda <45008948+vinodpalleda@users.noreply.github.com>
2022-11-09 23:02:49 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World! ci process checking";
 
     public App() {}
 
